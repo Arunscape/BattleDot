29b7286bc2086a3a7e8bcbbb18d9a0896c1b8dbb
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-03-23 15:21:32 -0600

    stuff

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
+from typing import List
+import random
 
 class Board:
     """
-    O is the player's dot
-    ■ is an empty space
+    ■ is an unmarked space
     X is a miss (wrong guess)
     """
     def __init__(self):
         self.board = [["■" for _ in range(10)] for _ in range(10)]
+        self.dot = (random.randint(0, 9), random.randint(0,9))
 
     def print(self) -> None:
         for row in self.board:
             print(" ".join(row))
 
-    def update(self, x: int, y: int, value: str) -> None:
+    def update_board(self, x: int, y: int, value: str) -> None:
         """
         0,0 is top left
         9,9 if bottom right
         """
         self.board[y][x] = value
+        self.print()
 
+    def send_bomb(self, x: int, y: int):
+        self.update_board(x, y, "X")
+        # TODO IPC
+        pass
+
+    def receive_bomb(self, x: int, y: int):
+        if self.dot == (x,y):
+            print("I WAS HIT")
 
 
 if __name__ == "__main__":
     b = Board()
     b.print()
     print()
-    b.update(0,0,"X")
-    b.print()
-    print()
-    b.update(9,9,"X")
-    b.print()
-    print()
-    b.update(7,2,"X")
-    b.print()
