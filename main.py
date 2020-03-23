238795e8af99f2a516013a418a1b71d049b3803b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-03-23 17:23:56 -0600

    i don't know

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from typing import List
 import random
+import socket
+import socketserver
 
 class Board:
     """
@@ -32,8 +34,38 @@ class Board:
         if self.dot == (x,y):
             print("I WAS HIT")
 
+class Player(socketserver.StreamRequestHandler):
+    def handle(self):
+        print(f"Received request from {self.client_address}")
+        message = self.rfile.readline().strip()
+        print(f"Data received is {message}")
+
 
 if __name__ == "__main__":
-    b = Board()
-    b.print()
-    print()
+
+    PLAYERS = []
+    BOARD = Board()
+
+    print("Welcome to BattleDot, an unpopular networked spinoff of the popular Battleship game.")
+
+    first = input("Are you the first player? (y/n): ")
+    if first == "y":
+        first = True
+        PLAYERS.append(1)
+    else:
+        first = False
+
+    if not first:
+        destination = input("Choose the destination address. If you were sitting at a table, this person would be on your right: ")
+        destination_port = input("Choose the port for the destination: ")
+        destination_port = int(destination_port)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((destination, destination_port))
+        s.sendto("HELLO".encode(), (destination, destination_port))
+        s.recv(1024)
+        s.close()
+
+    port = input("Choose the port to run this application on: ")
+    port = int(port)
+    server = socketserver.TCPServer(("127.0.0.1", port), Player)
+    server.serve_forever()
